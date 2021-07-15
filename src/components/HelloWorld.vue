d05592b51b3f3e1c3b2ac37245a61f471b09f5c9
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2021-07-16 00:06:33 +0200

    TinDog app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,88 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <p>Hola</p>
+  <div class="display">
+    <img :src="url" alt="A dog" class="main-img" />
+    <button @click="Next" class="next">></button>
+  </div>
+  <button @click="Like">Like</button>
+  <div class="dogs-container">
+    <span v-for="(dog, index) in likedDogs" :key="dog" class="dogCard">
+      <img :src="dog.imgSrc" class="liked-img" />
+      <button @click="Dislike(index)">🗑</button>
+    </span>
   </div>
 </template>
-
+Place the like button on the right-bottom of the image Change the arrow style
+and add arrow press functionality Give more style to the liked dogs Research
+about a share img option
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      arrow: "<",
+      url: "",
+      likedDogs: [],
+    };
+  },
+  created() {
+    axios.get("https://dog.ceo/api/breeds/image/random").then((result) => {
+      this.url = result.data.message;
+    });
+  },
+  methods: {
+    Next() {
+      axios.get("https://dog.ceo/api/breeds/image/random").then((result) => {
+        this.url = result.data.message;
+      });
+    },
+    Like() {
+      this.likedDogs.push({
+        imgSrc: this.url,
+        breed: "Dog",
+      });
+      this.Next();
+    },
+    Dislike(index) {
+      this.likedDogs.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.display {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.main-img {
+  width: 500px;
+  height: 400px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.next {
+  margin-left: -25px;
 }
-a {
-  color: #42b983;
+.liked-img {
+  width: 105px;
+  height: 90px;
+  margin: 10px;
+}
+.dogs-container {
+  height: 175px;
+  margin-top: 60px;
+  display: flex;
+  overflow-x: auto;
+}
+.dogCard {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 135px;
+  width: 105px;
+  margin: 0px 10px;
 }
 </style>
